3e85b7f40fccca0b1ca300e98d66f6f980bdc7db
Hankaji <hankaji135@gmail.com>
2026-01-10 02:14:41 +0700

    feat: implement closed feedback loop for motor sim

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
 #include "motor-simulator/motorSimulator.h"
+#include <algorithm>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
+
   MotorSimulator motor;
+
+  const int targetADC = 700;
+  const int tolerance = 3;
+  const float step = 0.0008f;
+
+  float pwm = 0.45f; // initial guess (45%)
+  motor.setPWM(pwm);
+
+  std::cout << "Target ADC = " << targetADC << "\n";
+  std::cout << "Starting feedback control...\n";
+
+  int loop = 1;
+
+  while (true) {
+    int adc = motor.getADC();
+    int error = targetADC - adc;
+
+    std::cout << "[Loop " << loop << "] "
+              << "PWM = " << static_cast<int>(pwm * 100) << "%, "
+              << "ADC = " << adc << ", "
+              << "Error = " << (error > 0 ? "+" : "") << error << "\n";
+
+    if (std::abs(error) <= tolerance) {
+      std::cout << "\nTarget reached! Final PWM = "
+                << static_cast<int>(pwm * 100) << "%\n";
+      break;
+    }
+
+    // Change PWN proportionally to error value, but never has lower than 1%
+    // gain
+    float deltaPWM = std::max(step * error, 0.01f);
+    pwm += deltaPWM;
+    motor.setPWM(pwm);
+    loop++;
+  }
 
   return 0;
 }
